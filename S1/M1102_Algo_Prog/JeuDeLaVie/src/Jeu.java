8fd3018e5a4ca7280aabee4842cf4461e4941165
aquadzn <williamjcqs8@gmail.com>
2020-12-24 19:56:34 +0100

    bonne version

--- S1/M1102_Algo_Prog/JeuDeLaVie/src/Jeu.java
+++ S1/M1102_Algo_Prog/JeuDeLaVie/src/Jeu.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 
 public class Jeu {
 
+    // Code ANSI pour la couleur dans le terminal
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    /**
+     * Lance le jeu.
+     */
     public static void lancer() {
 
         Scanner saisie = new Scanner(System.in);
@@ -14,21 +18,21 @@ public class Jeu {
         int niveau = 0;
 
         nettoyerConsole();
-        remplirTableau(tab, 0.1);
+        remplirTableau(tab, Main.ALEATOIRE);
 
         afficherTableau(tab, niveau);
 
-        System.out.print("Valider votre choix avec 'y' : --> ");
+        afficherParametres();
         String choix = saisie.nextLine();
 
         while (!choix.equals("y")) {
 
-            remplirTableau(tab, 0.1);
+            remplirTableau(tab, Main.ALEATOIRE);
 
             nettoyerConsole();
             afficherTableau(tab, niveau);
 
-            System.out.print("Valider votre choix avec 'y' : --> ");
+            afficherParametres();
             choix = saisie.nextLine();
         }
 
@@ -51,93 +55,131 @@ public class Jeu {
         }
     }
 
+    /**
+     * Affiche les paramètres du jeu utilisés
+     * et demande la validation pour lancer le jeu.
+     */
+    public static void afficherParametres() {
+        System.out.println("Paramètres: " + Main.LONGUEUR + "x" + Main.HAUTEUR + ", " + (Main.ALEATOIRE * 100) + "% de vivants");
+        System.out.print("Valider votre choix avec 'y' : --> ");
+    }
 
+    /**
+     * Compte le nombre de voisins dans un tableau
+     * pour une ligne et une colonne donnée.
+     * Retourne un entier.
+     * 
+     * @param tab tableau 2D à parcourir
+     * @param ligne ligne à parcourir
+     * @param colonne colonne à parcourir
+     * @return nombre de voisins trouvés
+     */
     public static int calculVoisins(int[][] tab, int ligne, int colonne) {
         int voisins = 0;
 
         for (int x = -1; x <= 1; x++) {
             for (int y = -1; y <= 1; y++) {
-                if (tab[ligne + x][colonne + y] == Main.VIVANT) {
+                if (tab[ligne + x][colonne + y] == 1) {
                     voisins += 1;
                 }
             }
         }
 
-        if (tab[ligne][colonne] == Main.VIVANT) {
+        if (tab[ligne][colonne] == 1) {
             voisins -= 1;
         }
 
         return voisins;
     }
 
-
+    /**
+     * Complète une évolution pour un tableau d'entier donné
+     * en calculant pour chaque élément du tableau ses voisins
+     * puis en appliquant les règles du jeu pour cet élément.
+     * Retourne un nouveau tableau d'entier après évolution.
+     * 
+     * @param tab tableau 2D à parcourir
+     * @return tableau 2D après évolution
+     */
     public static int[][] evolution(int[][] tab) {
-
+        
         int[][] nouveau_tab = new int[Main.HAUTEUR][Main.LONGUEUR];
 
-        // Loop through every cell
-        for (int l = 1; l < Main.HAUTEUR - 1; l++) {
-            for (int m = 1; m < Main.LONGUEUR - 1; m++) {
-                int voisins = calculVoisins(tab, l, m);
+        for (int ligne = 1; ligne < Main.HAUTEUR - 1; ligne++) {
+            for (int colonne = 1; colonne < Main.LONGUEUR - 1; colonne++) {
+                int voisins = calculVoisins(tab, ligne, colonne);
 
-                // Implementing the Rules of Life
+                // Règles du jeu
+                // =============
+                // Cellule vivante
+                if (tab[ligne][colonne] == 1) {
+                    // Si <2 voisins ou >3 voisins Alors morte sinon pareil
+                    nouveau_tab[ligne][colonne] = (voisins < 2 || voisins > 3) ? 0 : tab[ligne][colonne];
+                }
+                // Cellule morte
+                else {
+                    // Si exactement trois voisins Alors vivante sinon pareil
+                    nouveau_tab[ligne][colonne] = (voisins == 3) ? 1 : tab[ligne][colonne];
+                }
 
-                // Cell is lonely and dies
-                if ((tab[l][m] == Main.VIVANT) && (voisins < 2))
-                    nouveau_tab[l][m] = Main.MORT;
-
-                    // Cell dies due to over population
-                else if ((tab[l][m] == Main.VIVANT) && (voisins > 3))
-                    nouveau_tab[l][m] = Main.MORT;
-
-                    // A new cell is born
-                else if ((tab[l][m] == Main.MORT) && (voisins == 3))
-                    nouveau_tab[l][m] = Main.VIVANT;
-
-                    // Remains the same
-                else
-                    nouveau_tab[l][m] = tab[l][m];
             }
         }
 
         return nouveau_tab;
     }
-
-
+ 
+    /**
+     * Nettoie la console.
+     * Ne fonctionne pas sur les terminaux ne supportant pas ANSI.
+     * https://stackoverflow.com/a/32295974
+     */
     public static void nettoyerConsole() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
         System.out.println();
     }
 
-
+    /**
+     * Rempli un tableau 2D donnée de façon aléatoire
+     * avec des 0 et des 1 selon une probabilité donnée.
+     * 
+     * @param tab tableau 2D à parcourir
+     * @param valeur probabilité utilisée pour 1
+     */
     public static void remplirTableau(int[][] tab, double valeur) {
         for (int ligne = 0 ; ligne < tab.length ; ligne++) {
             for (int colonne = 0 ; colonne < tab[ligne].length ; colonne++){
                 if (Math.random() < valeur) {
-                    tab[ligne][colonne] = Main.VIVANT;
+                    tab[ligne][colonne] = 1;
                 }
                 else {
-                    tab[ligne][colonne] = Main.MORT;
+                    tab[ligne][colonne] = 0;
                 }
             }
         }
     }
 
-
+    /**
+     * Affiche un le jeu, le niveau actuel ainsi que la population
+     * à partir d'un tableau 2D et d'un niveau donnés.
+     * Remplace les 0 et les 1 par des symboles ASCII colorés.
+     * 
+     * @param tab tableau 2D à parcourir
+     * @param niveau niveau actuel
+     */
     public static void afficherTableau(int[][] tab, int niveau) {
         int population = 0;
         for (int ligne = 0 ; ligne < Main.HAUTEUR ; ligne++) {
             for (int colonne = 0 ; colonne < Main.LONGUEUR ; colonne++){
                 switch (tab[ligne][colonne]) {
                     case 0:
-                        System.out.print(ANSI_WHITE_BACKGROUND + "⠀" + ANSI_RESET);
+                        System.out.print(ANSI_WHITE_BACKGROUND + Main.MORT + ANSI_RESET);
                         break;
                     case 1:
-                        System.out.print(ANSI_WHITE_BACKGROUND + ANSI_BLACK + "█" + ANSI_RESET);
+                        System.out.print(ANSI_WHITE_BACKGROUND + ANSI_BLACK + Main.VIVANT + ANSI_RESET);
                         break;
                 }
-                if (tab[ligne][colonne] == Main.VIVANT) {
+                if (tab[ligne][colonne] == 1) {
                     population++;
                 }
             }
--- S1/M1102_Algo_Prog/JeuDeLaVie/src/Main.java
+++ S1/M1102_Algo_Prog/JeuDeLaVie/src/Main.java
@@ -1,15 +1,96 @@
 public class Main {
 
     // Paramètres du jeu
-    public static final int HAUTEUR = 20;
-    public static final int LONGUEUR = 60;
-//    public static final char VIVANT = '#';
-//    public static final char MORT = '·';
-    public static final int VIVANT = 1;
-    public static final int MORT = 0;
+    public static int LONGUEUR = 60;
+    public static int HAUTEUR = 20;
+    public static double ALEATOIRE = 0.2;
+    // Constantes
+    public static final char MORT = '_';
+    public static final char VIVANT = '#';
 
+    /**
+     * Programme principal du jeu de la vie.
+     * 
+     * @param args arguments CLI
+     */
     public static void main(String[] args) {
-        Jeu jeu = new Jeu();
-        jeu.lancer();
+        menu(args);
     }
+
+    /**
+     * Affiche le menu principal du jeu.
+     * 
+     * @param args paramètres optionnels du jeu
+     */
+    public static void menu(String args[]) {
+        if (args.length == 0) {
+            Jeu jeu = new Jeu();
+            jeu.lancer();
+        }
+        else {
+            if (args[0].equals("help")) {
+                System.out.println("Main.class");
+                System.out.println("==========");
+                System.out.println("\nUtilisation");
+                System.out.println("===========");
+                System.out.println("\tjava Main");
+                System.out.println("\tjava Main 80");
+                System.out.println("\tjava Main 80 30");
+                System.out.println("\tjava Main 80 0.3\n");
+                System.out.println("Arguments optionnels");
+                System.out.println("====================");
+                System.out.println("\tLONGUEUR [int] : Longueur de la fenêtre de jeu");
+                System.out.println("\tHauteur [int] : Hauteur de la fenêtre de jeu");
+                System.out.println("\tALEATOIRE [double] : Valeur aléatoire cellules vivantes (0-1)\n");
+            }
+            else {
+                switch (args.length) {
+                    case 1:
+                        LONGUEUR = Integer.parseInt(args[0]);
+                        break;
+                    case 2:
+                        LONGUEUR = Integer.parseInt(args[0]);
+                        HAUTEUR = Integer.parseInt(args[1]);
+                        break;
+                    case 3:
+                        LONGUEUR = Integer.parseInt(args[0]);
+                        HAUTEUR = Integer.parseInt(args[1]);
+                        ALEATOIRE = Double.parseDouble(args[2]);   
+                        break;
+                    default:
+                        break;
+                }
+
+                if (!verifierValeurs(LONGUEUR, HAUTEUR, ALEATOIRE)) {
+                    Jeu jeu = new Jeu();
+                    jeu.lancer();
+                }
+            }
+        }
+    }
+
+    /**
+     * Vérifie que les valeurs optionnels entrées sont correctes.
+     * 
+     * @param longueur longueur du jeu dans le terminal
+     * @param hauteur hauteur du jeu dans le terminal
+     * @param aleatoire valeur aléatoire pour remplir le tableau
+     * @return faux si pas d'erreur vrai si erreur
+     */
+    public static boolean verifierValeurs(int longueur, int hauteur, double aleatoire) {
+        boolean erreur = false;
+
+        if (longueur < 4 || hauteur < 4) {
+            erreur = true;
+            System.out.println("Les dimensions doivent avoir une valeur de 4x4 au minimum");
+        }
+
+        if (aleatoire <= 0. || aleatoire >= 1.) {
+            erreur = true;
+            System.out.println("La valeur aléatoire doit être comprise entre 0.0 et 1.0 exclus");
+        }
+
+        return erreur;
+    }
+
 }
