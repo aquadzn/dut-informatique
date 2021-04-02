acf5e9c443cff0788db560ccc44819f10ffcdc6a
William JACQUES <williamjcqs8@gmail.com>
2021-04-02 13:03:57 +0200

    TPs BDD

--- S2/BDD/TP06/TP06_wjacques.sql
+++ S2/BDD/TP06/TP06_wjacques.sql
@@ -5,11 +5,12 @@ create or replace view vueEns as
     select *
     from enseignant;
 
-delete from vueEns where idens = 99;
+select * from vueEns;
 insert into vueEns values (99, 'doe', 'john', 't', 'INFO');
 select * from vueEns;
--- update vueEns idens = 100 where nom like 'doe';
--- select * from vueEns;
+update vueEns set idens = 999 where nom like 'doe';
+select * from vueEns;
+delete from vueEns where idens = 999;
 
 -- 2.
 create or replace view vueNomPreEtud as
@@ -17,14 +18,25 @@ create or replace view vueNomPreEtud as
     from etudiant;
 
 select * from vueNomPreEtud;
+insert into vueNomPreEtud values ('doe', 'john');
+-- pas possible contrainte null pour les 2 autres attributs
+select * from vueNomPreEtud;
+update vueNomPreEtud set prenom = 'jean' where nom like 'doe';
+select * from vueNomPreEtud;
+delete from vueNomPreEtud where nom like 'doe';
 
 -- 3.
 create or replace view vueEtudS2 as
-    select nom, prenom
+    select *
     from etudiant
     where classe like 'S2';
 
 select * from vueEtudS2;
+insert into vueEtudS2 values (99, 'doe', 'john', 'S2');
+select * from vueEtudS2;
+update vueEtudS2 set prenom = 'jean' where nom like 'doe';
+select * from vueEtudS2;
+delete from vueEtudS2 where nom like 'doe';
 
 -- 4.
 create or replace view vueNotInf8 as
@@ -35,6 +47,12 @@ create or replace view vueNotInf8 as
     group by idinter;
 
 select * from vueNotInf8;
+-- erreur
+-- insert into vueNotInf8 values (99);
+-- select * from vueNotInf8;
+-- update vueNotInf8 set idinter = 999 where idinter = 99;
+-- select * from vueNotInf8;
+-- delete from vueNotInf8 where idinter = 99;
 
 -- 5.
 create or replace view vueMoyenEtud as
@@ -44,6 +62,7 @@ create or replace view vueMoyenEtud as
     group by nom, prenom;
 
 select * from vueMoyenEtud;
+-- erreur
 
 -- 6.
 create or replace view vueModInterro as
@@ -53,6 +72,13 @@ create or replace view vueModInterro as
         (select refmod from interro);
 
 select * from vueModInterro;
+insert into vueModInterro values ('M1199', 'Test', 'S1', 4.5, 10, 25, 25, 7);
+update vueModInterro set intitule = 'TEST' where refmod like 'M1199';
+--erreur update
+select * from vueModInterro;
+delete from vuemodinterro where refmod like 'M1199';
+-- erreur delete
+delete from module where refmod like 'M1199';
 
 -- 7.
 create or replace view vueModResp as
@@ -61,6 +87,7 @@ create or replace view vueModResp as
     natural join module;
 
 select * from vueModResp;
+-- erreur
 
 -- 8.
 create or replace view vueEtudAbsent as
@@ -70,6 +97,7 @@ create or replace view vueEtudAbsent as
     where note is null;
 
 select * from vueEtudAbsent;
+-- erreur
 
 -- 9.
 create or replace view vueNonEns as
@@ -79,3 +107,5 @@ create or replace view vueNonEns as
         (select nom from enseignant);
 
 select * from vueNonEns;
+-- erreur update
+-- erreur delete
---
+++ S2/BDD/TP07/TP07.sql
@@ -0,0 +1,110 @@
+-- -- Ex 1
+-- create view vueEtudS2 as
+--     select *
+--     from etudiant
+--     where classe like 'S2';
+
+
+-- -- 1.
+-- insert into etudiant values (100, 'Toto', null, 'S2');
+-- insert into etudiant values (101, 'Truc', null, 'S4');
+
+-- -- 2.
+-- insert into vueEtudS2 values (200, 'Fourcade', 'Martin', 'S4');
+
+-- -- 3.
+-- select * from vueEtudS2 order by idetud;
+-- select * from etudiant order by idetud;
+
+-- -- 4.
+-- delete from vueEtudS2 where idetud = 200;
+-- drop view if exists vueEtudS2;
+
+-- -- 5.
+-- create view vueEtudS2 as
+--     select *
+--     from etudiant
+--     where classe like 'S2'
+--     with check option;
+
+-- insert into vueEtudS2 values (200, 'Fourcade', 'Martin', 'S4');
+-- delete from etudiant where idetud = 101;
+
+-- -- 6.
+-- select * from vueEtudS2 order by idetud;
+-- select * from etudiant order by idetud;
+-- -- permet de modifier les données cachées utilisées par une vue
+-- delete from etudiant where idetud = 100;
+-- delete from etudiant where idetud = 200;
+
+-- Ex 2
+
+drop view if exists vuePersonnel;
+
+-- 1.
+create view vuePersonnel as
+    select *
+    from personnel;
+
+-- 2.
+select * from vuePersonnel;
+
+-- 3.
+alter table personnel
+    add column if not exists dept varchar(32) default 'Informatique';
+
+-- 4.
+select * from personnel;
+
+-- 5.
+select * from vuePersonnel;
+-- vue pas à jour avec la table personnel
+
+-- 6.
+drop view if exists vuePersonnel;
+
+-- 7.
+create view vuePersonnel as
+    select *
+    from personnel;
+
+-- 8.
+select * from vuePersonnel;
+
+-- 9.
+ALTER TABLE personnel
+    ALTER COLUMN dept
+        SET DEFAULT 'IUT';
+
+-- 10.
+insert into personnel values (100, 'Toto', null, 'chomeur');
+
+-- 11.
+insert into vuePersonnel values (101, 'Titi', null, 'chomeur');
+
+-- 12.
+select * from personnel;
+select * from vuePersonnel;
+-- Pareil
+
+-- 13.
+alter view vuePersonnel rename to listPersonnel;
+
+-- 14.
+alter table personnel
+    alter column dept
+        drop default;
+
+-- 15.
+insert into listPersonnel values (102, 'Tata', null, 'chomeuse');
+
+-- 16.
+select * from personnel;
+
+-- 17.
+delete from personnel where idpers in (100, 101, 102);
+
+-- 18.
+EXECUTE SELECT 'DROP VIEW ' || table_name || ';'
+    FROM information_schema.views
+    WHERE table_schema = 'public' AND table_name != 'vueEtudS2'; 
